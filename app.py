b3009c17447578b3d3c9fae2fa760d00f8fa1426
Pinjala Hema Chandra <pinjalahemachandra469@gmail.com>
2026-05-25 22:21:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import subprocess
 
 st.title("🧩 NETWALK – Human vs Computer")
 
 st.write("DAA Puzzle Game using DFS, BFS and Greedy Algorithms")
 
-if st.button("Run Game"):
-    st.success("Game Started!")
+if st.button("Start Game"):
+    subprocess.run(["python", "DAA_R1.py"])
